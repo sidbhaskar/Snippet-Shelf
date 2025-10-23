619825dfb44dcb59258e38ff0b3b86045c76af48
Siddharth Bhaskar <sidbhaskar1111@gmail.com>
2025-10-23 16:08:22 +0530

    Initial commit

---
+++ Snippet Shelf Backend/src/main/java/com/ss/snippet_shelf_backend/SnippetShelfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ss.snippet_shelf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetShelfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetShelfBackendApplication.class, args);
+	}
+
+}
---
+++ Snippet Shelf Backend/src/test/java/com/ss/snippet_shelf_backend/SnippetShelfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.snippet_shelf_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetShelfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
